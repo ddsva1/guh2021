ab26996c4d0fce54cf165439b89d484f0734e106
June Choi <junny7020@gmail.com>
2021-11-27 18:11:46 +0000

    Level 3 added

--- mainfile.py
+++ mainfile.py
@@ -8,12 +8,39 @@ hearts = 1
 #pygame?? or tkinter if need be
 #
 # Why u so clever?
+
+def level2():
+    global hearts
+    print("Date 1")
+    print("*The First Date*") #make this text big
+    print("awwwww") #make this text small
+    print("Choose A  Movie")
+    print("a: twilight")
+    print("b: world war z")
+    print("c: Garfield: The Movie")
+    c = input("movie choice: ")
+    if c == "a":
+        print("Zombie loves this movie!")
+        print("shares some of their brainy popcorn")
+        print("zombie and you had a great movie date")
+        hearts = hearts + 1
+
+    elif c == "b":
+        print("zombie thinks you are immature")
+        
+        hearts = hearts - 1
+    elif c == "c":
+        print("zombie thinks youre insensitive (yikes)")
+        gameoverbad2()
+    else:
+        print('get error checking - note')
+
+
+
 def gameover():
-    print('--------------------------------------------')
     exit()
 
 def gameovergood():
-    print('--------------------------------------------')
     print("You became a zombie!")
     print("You and Tom the zombie got married")
     print("You and Tom got a house on a fixed inerest mortgage rate!")
@@ -26,7 +53,6 @@ def gameovergood():
     print("the end")
 
 def gameoverbad1():
-    print('--------------------------------------------')
     #play song - really really american one - name??
     print("wow you killed a zombie... his name was Tom")
     print("*child runs in the scene")
@@ -37,19 +63,20 @@ def gameoverbad1():
     gameover()
 
 def gameoverbad2():
-    print('--------------------------------------------')
     print("you've been cancelled on twitter")
     print("A large karen voice")
     print("game over")
+    gameover()
 
 def gameoverbad3():
-    print('--------------------------------------------')
     #death by angry cow
-    print("death by angry cow i guess!!")
+    print("Cow just killed you. Well he was holding a gun")
+    print("game over")
+    gameover()
 
 
 def level2():
-    print('--------------------------------------------')
+    global hearts
     print("Date 1")
     print("*The First Date*") #make this text big
     print("awwwww") #make this text small
@@ -63,19 +90,44 @@ def level2():
         hearts = hearts + 1
     elif c == "b":
         print("zombie thinks you are immature")
-        hearts = hearts
+        hearts = hearts -1
     elif c == "c":
         print("zombie thinks youre insensitive (yikes)")
         gameoverbad2()
     else:
         print('get error checking - note')
 
+def level3():
+    global hearts
+    print("Date 2")
+    print("*The Second Date")
+    print("You and Zombie is going to cemetery")
+    print("Look!! a cow in a coat. It's holding a gun!!")
+    print("It seems like you have a gun with you. What would you do?")
+    print("a: shoot the cow")
+    print("b: run away")
+    print("c: don't shoot. It can be friendly")
+    c = input("action choice:")
+    if c == "a":
+        hearts = hearts +1
+        print("You and the zombie is having some burgers for dinner")
+    elif c =="b":
+        hearts = hearts -1
+        print("You are shot! But still got away from the nasty cow.")
+    elif c == "c":
+        print("The cow attacks you!")
+        gameoverbad3()
+    else:
+        print("You did nothing")
+
+
+
+
 def start():
     global hearts
     hearts = 1
     print("press a, b, c for each option choice")
     inp = input("press enter key to start game ")
-    print('--------------------------------------------')
     if inp == "":
         print(":D")
         print("the background!")
@@ -97,25 +149,21 @@ def start():
                 print("You chose bat!")
                 print("You ht the zombie!")
                 print("boink!") #zombie is hit
-                print("Didn't work... zombie is mildly annoyed")
-                hearts = 1
+                print("Didn't work... zomvie is mildly annoyed")
             elif weapon == "b":
                 print("You chose a gun")
                 print("You shoot the zombie!")
                 print("Blood is splattered!")
-                print("Zombie's lifeless body falls to the floor")
+                print("Zombie lifeless body falls to the floor")
                 hearts = 0
             elif weapon == "c":
                 print("You chose a crossbow")
-                print("You are inexperienced!")
+                print("You are inexprinced!")
                 print("You shot yourself in the foot")
                 print("zombie smells your blood, looks hungry")
-                hearts = 1
             elif weapon == "d":
                 #wow brave choice etc etc
                 print("you're going into this empty handed")
-                #the zombie approves etc and thinks youre being friendly
-                hearts = 2
             else:
                 print("sorry try again, that wasnt one of the options?")
 
@@ -123,7 +171,7 @@ def start():
     else:
         print('wow')
         print("coward")
-        print('--------------------------------------------')
+        exit()
 
 start()
 
